605a5383ec8057188baa7dd32237a9cfef40c207
kaustubh mishal <mishal.kaustubh@gmail.com>
2019-06-12 00:47:23 +0530

    Add Browser popup

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,4 +2,38 @@
 (function() {
 
     // Write your custom script here!!
+    var submitCtaBtn = document.getElementById('submit-cta');
+    submitCtaBtn.addEventListener('click', function() {
+        getBrowserInfo();
+    });
 })();
+
+
+function getBrowserInfo(){
+    var getUserAgentInfo = navigator.userAgent
+    var findBrowrserName = getUserAgentInfo.split(' ');
+    var popup = document.querySelector('.popup');
+    findBrowrserName.forEach( function( name ){
+        var nametoLowerCase = name.toLowerCase();
+        // If browser is Edge
+        if(nametoLowerCase.includes("chrome") && nametoLowerCase.includes("edge")){
+            popup.style.bottom = '20px';
+            popup.style.left = '50%';
+            popup.style.right = '50%';
+            popup.style.transform = 'translate(-50%, 0)'
+            popup.style.display = 'block'; 
+        }
+        // If browser is Firefox
+        else if(nametoLowerCase.includes("firefox")){
+            popup.style.top = '20px';
+            popup.style.left = '20px';
+            popup.style.display = 'block'; 
+        }
+         // If browser is Chrome
+        else if(nametoLowerCase.includes("chrome") || nametoLowerCase.includes("edge")){
+            popup.style.top = '20px';
+            popup.style.right = '20px';
+            popup.style.display = 'block'; 
+        }
+    });
+}
